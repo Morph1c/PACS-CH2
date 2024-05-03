5d6b9e15fc74e91f7e75003beda85e58fe4b28f0
“ <andreatocca2503@gmail.com>
2024-05-03 10:18:13 +0000

    added very small test

--- test/main.cpp
+++ test/main.cpp
@@ -11,6 +11,8 @@ int main(int argc, char* argv[]) {
   using type_format = double;
   std::string file_name = "./lnsp_131.mtx";
   std::string file_name_small = "./small_example.mtx";
+  std::string file_name_very_small = "./very_small_example.mtx";
+
   std::string complex_file_name = "./lnsp_511.mtx";
 
 
@@ -24,8 +26,8 @@ int main(int argc, char* argv[]) {
 
   // basic operations test
   //bench.test_basic_operations(complex_file_name);
-  bench.test_multiplication_correctness(file_name_small);
-
+  bench.test_multiplication_correctness(file_name_very_small);
+  //bench.test_norm(file_name_very_small);
   // Large benchmark test with lots of runs
   //bench.test_multiplication_correctness(file_name_small);
   //bench.medium_benchmark_multiplication(1);
